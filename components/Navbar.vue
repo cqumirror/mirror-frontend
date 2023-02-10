eecd5959dfcc073f0841e14993486c227a65089d
齐彬烨 <qibinye@inspur.com>
2023-02-10 11:38:23 +0800

    add responsive nav menu

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="navbar">
-      <a class="nav-brand" href="https://mirrors.cqu.edu.cn">重庆大学开源镜像站</a>
-      <div class="nav-right">
-        <ul class="nav">
-          <li v-for="item in menu" :key="item.key">
-            <NuxtLink :to="item.index">{{ item.label }}</NuxtLink>
-          </li>
-        </ul>
-      </div>
-      <button class="nav-toggle">&#8943</button>
+    <div class="nav-container">
+      <a class="nav-brand" href="https://mirrors.cqu.edu.cn">
+        11111
+      </a>
+      <button class="nav-toggle" @click="handleToggle">&#8943</button>
+    </div>
+    <div class="nav-right">
+      <ul class="nav">
+        <li v-for="item in menu" :key="item.key">
+          <NuxtLink :to="item.index" :custom="true">
+            <a @click="handleToggle">
+              {{ item.label }}
+            </a>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -18,6 +26,7 @@ export default {
   name: "Navbar",
   data() {
     return {
+      isOpened: false,
       activeIndex: '/',
       menu: [
         {
@@ -43,13 +52,32 @@ export default {
           url: "https://mirrorz.org"
         }
 
-      ]
+      ],
+      height: '',
     }
   },
   methods: {
     handleSelect(key, keyPath) {
       console.log(key, keyPath)
     },
+    handleToggle(e) {
+      // TODO 非常迷惑的处理方式，没想到其他的方法，我菜
+      const child = document.getElementsByClassName('nav-right')
+      if (this.isOpened) {
+        child[0].style.height = '0'
+        this.isOpened = !this.isOpened
+      } else {
+        child[0].style.height = this.height
+        this.isOpened = !this.isOpened
+      }
+
+    },
+    computeHeight() {
+      this.height = (this.menu.length * 57).toString() + 'px'
+    }
+  },
+  created() {
+    this.computeHeight()
   }
 }
 </script>
