5f9725d80daaee958ed45608f96a12b81b474db1
qinshurui <glfgpbt@163.com>
2023-02-19 21:37:10 +0800

    Wikipage

--- pages/wiki/index.vue
+++ pages/wiki/index.vue
@@ -1,28 +1,32 @@
 <template>
-  <div style="margin: 0;padding: 0">
-    <h1>Wiki Content</h1>
+  <el-container class="wiki-container"> 
+    <el-aside style="margin: 0 auto; width: 13rem;">
     <ul>
-      <template v-for="article in articles">
-        <li :key="article.slug">
-          <NuxtLink :to="{name: 'wiki-slug', params: { slug: article.slug }}">
-            <div>
-              <h2>
-                {{ article.title }}
-              </h2>
+      <template v-for="(article,k) in articles">
+        <li :key="article.slug" :class="{active : isActive === k}" @click="toActive(k)" >
+          <NuxtLink :to="{name: 'wiki-index-slug', params: { slug: article.slug } }" class="wiki-content">
+            <div class="wiki-title">
+                {{ article.slug }}
             </div>
           </NuxtLink>
         </li>
       </template>
     </ul>
-  </div>
+  </el-aside>
+  <el-main>
+    <nuxt-child/>
+  </el-main>
+  </el-container>
 </template>
 
 <script>
+
 export default {
   name: "wiki",
   data() {
     return {
       articles: [],
+      isActive: 0,
     }
   },
   async asyncData({ $content, params }) {
@@ -33,10 +37,47 @@ export default {
     return {
       articles
     }
+  },
+  methods:{
+    toActive(k){
+      this.isActive = k;
+    }
   }
 }
 </script>
 
 <style scoped>
+li {
+  list-style: none;
+  height: 2rem;
+  margin-top:0.1rem ;
+  padding-top:0.8rem ;
+  border-radius: 0.3rem;
+}
+a {
+  text-decoration: none;
+}
+.active {
+  background-color: #53CAFF;
+}
+li .nuxt-link-active {
+  color: #fff;
+}
+.wiki-content {
+  
+  color: #53CAFF;
+}
 
+
+.wiki-title{
+  height: 1rem;
+  font-size: 1rem;
+  line-height: 1rem;
+  margin-left: 1rem;
+  font-weight: 400;
+}
+.wiki-container{
+  margin-left: 10vw;
+
+}
 </style>
