d0fc7e8395fdf23a09e6111a818490aacee5fc27
Binye Qi <qby19981121@gmail.com>
2023-09-27 02:11:47 +0800

    Doc debian (#44)
    * update function: domain whitelist and url push

--- components/scroll-notice/warpNotice.vue
+++ components/scroll-notice/warpNotice.vue
@@ -26,13 +26,47 @@ export default {
     center: { type: Boolean, default: false },
     showIcon: { type: Boolean, default: true },
     effect: { type: String, default: 'light' },
-    callback: { type: String, default: undefined }
+    callback: { type: String, default: undefined },
+    action: { type: String, default: 'news'}
   },
   methods: {
+    isDomainInWhitelist(url, whitelist) {
+      // 提取URL中的域名部分
+      const domainRegex = /^https?:\/\/([^/]+)\//;
+      const matches = url.match(domainRegex);
+
+      if (matches && matches.length > 1) {
+        const domain = matches[1];
+        // 检查域名是否在白名单中
+        return whitelist.includes(domain);
+      }
+
+      // 如果无法提取域名，默认为不在白名单中
+      return false;
+    },
     handleCallback() {
       if (this.$props.callback) {
-        console.log(this.$props.callback)
-        this.$router.push({name: 'news-page', params: { page: this.$props.callback }});
+        // console.log(this.$props)
+        let that = this
+        const prop = that.$props
+        if (prop.action) {
+          let action = prop.action
+          const whiteList = process.env.whiteListDomain
+          if (action === 'url') {
+            // 必须进行校验
+            if (this.isDomainInWhitelist(prop.callback, whiteList)) {
+              window.open(prop.callback, '_blank')
+            } else {
+              return
+            }
+          } else if (action === 'news') {
+            this.$router.push({name: 'news-page', params: { page: this.$props.callback }})
+          } else if (action === 'wiki') {
+            this.$router.push({name: 'wiki', params: { page: prop.callback }})
+          } else {
+            this.$router.push({name: 'news-page', params: { page: this.$props.callback }})
+          }
+        }
       }
     }
   },
